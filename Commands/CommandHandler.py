3c4a79e5149c5028d51b2d8fcdec05c8976f5c34
GlivchGriefer <calvin.mack@ymail.com>
2020-08-16 23:14:51 -0600

    v0.2

---
+++ Commands/CommandHandler.py
@@ -0,0 +1,72 @@
+# COMMAND HANDLER CLASS
+class CommandHandler:
+
+    # ON CREATION, RECORD CLIENT VARIABLE
+    def __init__(self, client):
+        self.client = client
+
+        # CREATE ARRAY TO STORE COMMANDS
+        self.commands = []
+
+    def add_command(self, command):
+
+        # ADD A COMMAND TO THE COMMANDS ARRAY
+        self.commands.append(command)
+
+    def command_handler(self, message):
+
+        # LOOP THROUGH COMMAND ARRAY
+        for command in self.commands:
+
+            # IF MESSAGE STARTS WITH TRIGGER
+            if message.content.startswith(command['trigger']):
+                args = message.content.split(' ')
+                if args[0] == command['trigger']:
+                    args.pop(0)
+                    if command['args_num'] == 0:
+                        # RETURN RESULTS OF THE FUNCTION
+                        return self.client.send_message(message.channel,
+                                                        str(command['function'](message, self.client, args)))
+                        break
+                    else:
+                        if len(args) >= command['args_num']:
+                            # RETURN RESULTS OF THE FUNCTION
+                            return self.client.send_message(message.channel,
+                                                            str(command['function'](message, self.client, args)))
+                            break
+                        else:
+                            # RETURN ARGUMENT ERROR
+                            return self.client.send_message(message.channel,
+                                                            'command "{}" requires {} argument(s) "{}"'.format(
+                                                                command['trigger'], command['args_num'],
+                                                                ', '.join(command['args_name'])))
+                            break
+                else:
+                    break
+
+
+# MAIN FUNCTION
+def hello_function(message, client, args):
+    try:
+        return 'Hello {}, Argument One: {}'.format(message.author, args[0])
+    except Exception as e:
+        return e
+
+
+# COMMAND DICTIONARY
+ch.add_command({
+    # IF MESSAGE STARTS WITH TRIGGER
+    'trigger': '!hello',
+
+    # CALL MAIN FUNCTION
+    'function': hello_function,
+
+    # NUMBER OF ARGUMENTS NEEDED
+    'args_num': 1,
+
+    # NAME OF THE ARGUMENT
+    'args_name': ['string'],
+
+    # DESCRIBE WHAT FUNCTION DOES
+    'description': 'Will respond hello to the caller and show arg 1'
+})
--- bot.py
+++ bot.py
@@ -1,22 +1,48 @@
 # IMPORTS
-from discord.ext.commands import Bot
 import os
+import discord
+from discord.ext.commands import Bot
+from Commands.CommandHandler import CommandHandler
+
 
 # VARIABLES
 bot = Bot(command_prefix='-')
+ch = CommandHandler(bot)
 
 
 # BOT IS READY
 @bot.event
 async def on_ready():
-    print('All systems nominal.')
+    try:
+        # PRINT BOT INFORMATION TO CONSOLE
+        print(bot.user.name)
+        print(bot.user.id)
+        print('Discord.py Version: {}'.format(discord.__version__))
+        print('All systems nominal.')
+
+    except Exception as e:
+        print(e)
 
 
-# IGNORE MESSAGES FROM BOT
+# IGNORE MESSAGES FROM BOT | TRY COMMAND HANDLER
 @bot.event
-async def on_message(m):
-    if m.author == bot.user:
-        return
+async def on_message(message):
+    # if the message is from the bot itself ignore it
+    if message.author == bot.user:
+        pass
+    else:
+
+        # try to evaluate with the command handler
+        try:
+            await ch.command_handler(message)
+
+        # message doesn't contain a command trigger
+        except TypeError as e:
+            pass
+
+        # generic python error
+        except Exception as e:
+            print(e)
 
 
 bot.run(os.environ["BOT_TOKEN"])
