5fb7c11d88973c80a72f0e2e7c275d4c524b2812
GlivchGriefer <calvin.mack@ymail.com>
2020-08-17 00:45:07 -0600

    v0.4

--- bot.py
+++ bot.py
@@ -2,13 +2,10 @@
 import os
 import discord
 from discord.ext import commands
-from Commands.CommandHandler import CommandHandler
 
 
 # VARIABLES
 bot = commands.Bot(command_prefix='-')
-client = bot.user
-ch = CommandHandler(client)
 
 
 # BOT IS READY
@@ -26,30 +23,9 @@ async def on_ready():
 
 
 # ADD COMMANDS ---------------------------------------------------------------------------------------------------------
-# HELLO
-def hello_function(message, args):
-    try:
-        return 'Hello {}, Argument One: {}'.format(message.author, args[0])
-    except Exception as e:
-        return e
-
-
-ch.add_command({
-    # IF MESSAGE STARTS WITH TRIGGER
-    'trigger': '-hello',
-
-    # CALL MAIN FUNCTION
-    'function': hello_function,
-
-    # NUMBER OF ARGUMENTS NEEDED
-    'args_num': 1,
-
-    # NAME OF THE ARGUMENT
-    'args_name': ['string'],
-
-    # DESCRIBE WHAT FUNCTION DOES
-    'description': 'Will respond hello to the caller and show arg 1'
-})
+@bot.command()
+async def hello(ctx, arg):
+    await ctx.send(arg)
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -59,19 +35,6 @@ async def on_message(message):
     # if the message is from the bot itself ignore it
     if message.author == bot.user:
         pass
-    else:
-
-        # try to evaluate with the command handler
-        try:
-            await ch.command_handler(message)
-
-        # message doesn't contain a command trigger
-        except TypeError as e:
-            pass
-
-        # generic python error
-        except Exception as e:
-            print(e)
 
 
 bot.run(os.environ["BOT_TOKEN"])
