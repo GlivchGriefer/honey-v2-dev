19ca246e787252a8191fa6b5687719ed7b3ebe40
GlivchGriefer <calvin.mack@ymail.com>
2020-08-17 00:21:46 -0600

    v0.3

--- bot.py
+++ bot.py
@@ -25,6 +25,34 @@ async def on_ready():
         print(e)
 
 
+# ADD COMMANDS ---------------------------------------------------------------------------------------------------------
+# HELLO
+def hello_function(message, args):
+    try:
+        return 'Hello {}, Argument One: {}'.format(message.author, args[0])
+    except Exception as e:
+        return e
+
+
+ch.add_command({
+    # IF MESSAGE STARTS WITH TRIGGER
+    'trigger': '-hello',
+
+    # CALL MAIN FUNCTION
+    'function': hello_function,
+
+    # NUMBER OF ARGUMENTS NEEDED
+    'args_num': 1,
+
+    # NAME OF THE ARGUMENT
+    'args_name': ['string'],
+
+    # DESCRIBE WHAT FUNCTION DOES
+    'description': 'Will respond hello to the caller and show arg 1'
+})
+# ----------------------------------------------------------------------------------------------------------------------
+
+
 # IGNORE MESSAGES FROM BOT | TRY COMMAND HANDLER
 @bot.event
 async def on_message(message):
