3246d32ea8fcc39467923c1ebb50dc1a95670f7a
GlivchGriefer <calvin.mack@ymail.com>
2021-01-26 01:55:57 -0700

    v{}

--- bot.py
+++ bot.py
@@ -126,7 +126,6 @@ def list_submissions(sql2, ctx):
     :param sql2:
     :return:
     """
-    listsize = 0
     try:
         conn = psycopg2.connect(os.environ["DATABASE_URL"], sslmode='require')
         conn.autocommit = True
