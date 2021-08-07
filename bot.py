d3f90db90624f00f56061dd0fadb6eb911a751db
GlivchGriefer <calvin.mack@ymail.com>
2021-08-06 21:31:24 -0600

    v0.8.2[rr]

--- bot.py
+++ bot.py
@@ -321,11 +321,11 @@ async def rr(ctx):  # check role
         roles = member.roles
         if bad_role in roles:
             try:
-                await member.remove_role(member, bad_role)
+                await member.remove_roles(member, bad_role)
                 await ctx.send(f'Removed **TEST**', delete_after=5)
             except:
                 removal_failures += 1
-    print(f"Couldn't remove the role from {removal_failures} members.")
+    await ctx.send(f"Couldn't remove the role from {removal_failures} members.")
 
 # ------------------------------------------------------------------------------
 bot.add_cog(KeepClean(bot))
