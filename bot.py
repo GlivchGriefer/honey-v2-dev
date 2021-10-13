f2554562d3e3ebf34a55e57489dce8551dc82955
GlivchGriefer <43833196+GlivchGriefer@users.noreply.github.com>
2021-10-13 16:52:03 -0600

    v0.8.4.1[remove_rr_for_now]

--- bot.py
+++ bot.py
@@ -61,35 +61,6 @@ async def on_message(message):
         pass
     await bot.process_commands(message)
 
-msg = None
-collab = bot.get_emoji('894045982659072100')
-
-
-@bot.event  # Reaction Role (Collab)
-async def on_ready():
-    channel = bot.get_channel('709967233421410334')
-    global msg
-    msg = await channel.fetch_message('894048673342816307')
-    await bot.wait_for_reaction(emoji="<:collab:894048770411618324>"
-                                , message=msg)
-
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    if reaction.user == bot.user:
-        return
-    if reaction.message == msg and reaction.emoji == collab:
-        collaborator = discord.utils.get(bot.server.roles, name="Collaborator")
-        await bot.add_roles(user, collaborator)
-
-
-@bot.event
-async def on_reaction_remove(reaction, user):
-    if reaction.message == msg and reaction.emoji == collab:
-        collaborator = discord.utils.get(bot.server.roles, name="Collaborator")
-        await bot.remove_roles(user, collaborator)
-
-        
 # ------------------------------------------------------------------------------
 class KeepClean(commands.Cog):
     def __init__(self, bot):
