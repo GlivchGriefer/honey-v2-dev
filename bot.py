dd5ffa7214279f32e71821de82c95e7973347082
GlivchGriefer <43833196+GlivchGriefer@users.noreply.github.com>
2022-04-14 16:08:48 -0600

    v0.8.5.1[status]

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def on_ready():
     bot.remove_command('help')
 
     await bot.change_presence(activity=discord.Activity(
-        type=discord.ActivityType.listening, name="LFG .019"))
+        type=discord.ActivityType.listening, name="LFG .020 SPECIAL EDITION"))
 
 
 # ADMIN COMMANDS / EVENTS ------------------------------------------------------
